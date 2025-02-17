289c5127067ffb0ca4066968683be6cdfc98dc02
thando <tsgmagagula@gmail.com>
2025-02-17 21:05:29 +0200

    pwa

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,18 @@
-import type { NextConfig } from "next";
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = withPWA({
+  reactStrictMode: true,
   env: {
     NEXT_PUBLIC_SUPABASE_URL: "https://wngyegxnuzuyvzvliyen.supabase.co",
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InduZ3llZ3hudXp1eXZ6dmxpeWVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk0MzQ5NjAsImV4cCI6MjA1NTAxMDk2MH0.70EzqnALbUqc77I-FZupUNmT3GyT8QKdQ-mitTwERm0"
+    NEXT_PUBLIC_SUPABASE_ANON_KEY:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InduZ3llZ3hudXp1eXZ6dmxpeWVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk0MzQ5NjAsImV4cCI6MjA1NTAxMDk2MH0.70EzqnALbUqc77I-FZupUNmT3GyT8QKdQ-mitTwERm0",
   },
-};
+});
 
-export default nextConfig;
+module.exports = nextConfig;
